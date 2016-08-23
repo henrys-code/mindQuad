d4c66bfd0a6cbec06191e6a095dd0ccb99640cfc
Henry <hgaudet@ucsd.edu>
2016-08-22 23:32:05 -0400

    Serial connection to arduino now working

--- src/IMU_sketch.ino
+++ src/IMU_sketch.ino
@@ -6,43 +6,36 @@ states a conversion factor of 0.061 mg/LSB (least
 significant bit) at this FS setting, so the raw reading of
 16276 corresponds to 16276 * 0.061 = 992.8 mg = 0.9928 g.
 */
-
-#include <avr/pgmspace.h>
+#include <avr/pgmspace.h>   // Allows mem storage on flash instead of SRAM
 #include <Wire.h>
-#include <LSM6.h>
-#include <ros.h>
-#include <std_msgs/Int16.h>
+#include <LSM6.h>           // IMU library
+#include <Servo.h>          // Interfaces with motors
 
-ros::NodeHandle nh;
-std_msgs::Int16 msg;
-ros::Publisher imu_publisher("o", &msg);
+LSM6 imu;
 
 void setup()
 {
-    nh.initNode();
-    nh.advertise(imu_publisher);
+  Serial.begin(9600);
+  //imu.init();
+  //imu.enableDefault();
 }
 
 void loop()
 { 
-  
-  LSM6 imu;
-  msg.data = 47;
-  imu_publisher.publish(&msg);
+  //imu.read();
+  uint16_t ax = 10;
+  uint16_t ay = 24;
+  uint16_t az = 32;
+  uint16_t gx = 41;
+  uint16_t gy = 50;
+  uint16_t gz = 67;
 
-//  imu.init();
-//  imu.enableDefault();
-//  imu.read();
+  Serial.write(ax);
+  Serial.write(ay);
+  Serial.write(az);
+  Serial.write(gx);
+  Serial.write(gy);
+  Serial.write(gz);
   
-  /*
-  imu_msg.position.x = imu.a.x * 0.000061;
-  imu_msg.position.y = imu.a.y * 0.000061;
-  imu_msg.position.z = imu.a.z * 0.000061;
-
-  imu_msg.orientation.x = imu.g.x * 0.00875;
-  imu_msg.orientation.y = imu.g.y * 0.00875;
-  imu_msg.orientation.z = imu.g.z * 0.00875;
-  imu_msg.orientation.w = 0;
-  */
-  nh.spinOnce();
+  delay(1000);
 }
--- src/pyserial_test.py
+++ src/pyserial_test.py
@@ -2,9 +2,37 @@
 
 import rospy
 import serial
+from struct import unpack, pack
+from geometry_msgs.msg import Twist
 
-ser = serial.Serial('/dev/ttyUSB0')
-while (True):
-    data = ser.readline().strip()
-    print (data)
+class Arduino():
+    def __init__(self):
+        rospy.init_node('Arduino')
+        self.imu_publisher = rospy.Publisher(
+            "/imu_data",
+            Twist,
+            queue_size = 30
+        )
+        self.twist = Twist()
+        temp = [0]*6
+        ser = serial.Serial('/dev/ttyUSB0', 9600)
+        while not rospy.is_shutdown():
+            temp[0] = unpack('<B', ser.read())[0]
+            temp[1] = unpack('<B', ser.read())[0]
+            temp[2] = unpack('<B', ser.read())[0]
+            temp[3] = unpack('<B', ser.read())[0]
+            temp[4] = unpack('<B', ser.read())[0]
+            temp[5] = unpack('<B', ser.read())[0]
 
+            self.twist.linear.x = temp[0]
+            self.twist.linear.y = temp[1]
+            self.twist.linear.z = temp[2]
+            self.twist.angular.x = temp[3]
+            self.twist.angular.y = temp[4]
+            self.twist.angular.z = temp[5]
+            
+            self.imu_publisher.publish(self.twist)
+            rospy.sleep(1)
+
+if __name__ == '__main__':
+    Arduino()
